9d97132f0739f9212d7e3876e62f8f93ef2a64e4
Manuel <manu320cr@gmail.com>
2024-01-31 18:24:53 +0100

    Subida

---
+++ Práctica Tema 13 (RPG)CONSOLA/RPG.js
@@ -0,0 +1,213 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readlineSync = require("readline-sync");
+//CLASE JUGADOR
+var Jugador = /** @class */ (function () {
+    function Jugador(nombre) {
+        this.nombre = nombre;
+        this.puntos_salud = 20;
+        this.puntos_ataque = 0;
+        this.oro = 2;
+    }
+    Jugador.prototype.getNombre = function () {
+        return this.nombre;
+    };
+    Jugador.prototype.setNombre = function (nuevoNombre) {
+        this.nombre = nuevoNombre;
+    };
+    Jugador.prototype.getPuntos_salud = function () {
+        return this.puntos_salud;
+    };
+    Jugador.prototype.setPuntos_salud = function (nuevo_puntos_salud) {
+        this.puntos_salud = nuevo_puntos_salud;
+    };
+    Jugador.prototype.getPuntos_ataque = function () {
+        return this.puntos_ataque;
+    };
+    Jugador.prototype.setPuntos_ataque = function (puntos_ataque_nuevo) {
+        this.puntos_ataque = puntos_ataque_nuevo;
+    };
+    Jugador.prototype.getOro = function () {
+        return this.oro;
+    };
+    Jugador.prototype.setOro = function (nuevo_oro) {
+        this.oro = nuevo_oro;
+    };
+    Jugador.prototype.imprimirAtributos = function () {
+        console.log("Puntos de Salud: ".concat(this.puntos_salud));
+        console.log("Puntos de Ataque: ".concat(this.puntos_ataque));
+        console.log("Dinero: ".concat(this.oro));
+    };
+    Jugador.prototype.calcularFuerzaInicial = function () {
+        //Puntos de ataque aleatorios entre 1 y 15
+        this.puntos_ataque = Math.floor(Math.random() * 13) + 1;
+    };
+    return Jugador;
+}());
+//CLASE ENEMIGO
+var Enemigo = /** @class */ (function () {
+    function Enemigo(nombre) {
+        this.nombre = nombre;
+        this.puntos_ataque = 0;
+    }
+    // Métodos get y set (opcional)
+    Enemigo.prototype.getNombre = function () {
+        return this.nombre;
+    };
+    Enemigo.prototype.setNombre = function (nombre) {
+        this.nombre = nombre;
+    };
+    Enemigo.prototype.getPuntosAtaque = function () {
+        return this.puntos_ataque;
+    };
+    Enemigo.prototype.setPuntosAtaque = function (puntos_ataque) {
+        this.puntos_ataque = puntos_ataque;
+    };
+    Enemigo.prototype.calcularFuerzaEnemigo = function () {
+        // Establecer puntos de ataque aleatorios entre 1 y 10
+        this.puntos_ataque = Math.floor(Math.random() * 10) + 1;
+    };
+    Enemigo.prototype.soltarDinero = function () {
+        // Establecer la cantidad de dinero que soltará el enemigo de forma aleatoria
+        return Math.floor(Math.random() * 10) + 1;
+    };
+    return Enemigo;
+}());
+function Main() {
+    function historia() {
+        console.log("---------------------------------------");
+        console.log("Título del Juego: Medac`s Curse");
+        console.log("HISTORIA");
+        console.log("En la academia mágica de Medac, una maldición ha convertido a profesores");
+        console.log("y compañeros en oscuros enemigos sedientos de poder. Como estudiante");
+        console.log("resistente a la maldición, tu misión es liberar la academia y restaurar la paz.");
+        console.log("---------------------------------------");
+    }
+    var nombre = readlineSync.question('Ingresa tu nombre: ');
+    var jugador = new Jugador("".concat(nombre));
+    console.log("\u00A1Hola, ".concat(nombre, "!"));
+    jugador.calcularFuerzaInicial();
+    var opcion;
+    do {
+        historia();
+        console.log("1.Luchar contra el enemigo");
+        console.log("2.Comprar ítems");
+        console.log("3.Consultar estadisticas ");
+        console.log("4.Pagar 1 para generar nueva fuerza");
+        console.log("5.Exit");
+        opcion = readlineSync.questionInt('Ingresa tu eleccion:');
+        switch (opcion) {
+            case 1:
+                Luchar();
+                break;
+            case 2:
+                comprarItems();
+                break;
+            case 3:
+                console.log("Nombre: ".concat(nombre));
+                jugador.imprimirAtributos();
+                break;
+            case 4:
+                if (jugador.oro >= 5) {
+                    cambiarFuerza();
+                    break;
+                }
+                else {
+                    console.log("Si no tienes dinero pa que vienes");
+                    break;
+                }
+            case 5:
+                console.log("Nos vemos!!!");
+                return;
+            default:
+                console.log("Opción no válida. Por favor, elige nuevamente.");
+        }
+    } while (opcion !== 5);
+    function cambiarFuerza() {
+        jugador.calcularFuerzaInicial();
+        jugador.oro -= 1;
+        console.log("Tienes ".concat(jugador.oro, " oro"));
+    }
+    function Luchar() {
+        var enemigos = [
+            new Enemigo("Javi"),
+            new Enemigo("Evelyn"),
+            new Enemigo("Antonio")
+        ];
+        var nombreEnemigo = Math.floor(Math.random() * enemigos.length); //Al ser un numero el nombre sera la posicion en el array
+        enemigos[nombreEnemigo].calcularFuerzaEnemigo(); //Calculamos su fuerza
+        if (jugador.puntos_salud == 0) {
+            console.log("GAME OVER");
+            return;
+        }
+        if (jugador.puntos_ataque >= enemigos[nombreEnemigo].puntos_ataque) {
+            console.log("Has ganado!!!!!!");
+            jugador.oro += enemigos[nombreEnemigo].soltarDinero();
+            console.log("Tienes ".concat(jugador.oro, " oro"));
+        }
+        if (jugador.puntos_ataque < enemigos[nombreEnemigo].puntos_ataque) {
+            console.log("Has perdido ahahahahaa");
+            var vidaPerdida = (enemigos[nombreEnemigo].puntos_ataque) - (jugador.puntos_ataque);
+            jugador.puntos_salud -= vidaPerdida;
+            console.log("Has perdido ".concat(vidaPerdida, " y te quedan ").concat(jugador.puntos_salud));
+        }
+    }
+    function comprarItems() {
+        var opcion;
+        do {
+            console.log("1.Glock-18 por 5 de oro");
+            console.log("2.AK-47 por 15 de oro");
+            console.log("3.Venda por 5 de oro");
+            console.log("4.Botiquin por 10 de oro");
+            console.log("5.Exit");
+            opcion = readlineSync.questionInt('Ingresa tu eleccion:');
+            switch (opcion) {
+                case 1:
+                    if (jugador.oro >= 5) {
+                        jugador.puntos_ataque += 5;
+                        console.log("Se a\u00F1adio Glock-18 a tu inventario, tu ataque es de ".concat(jugador.puntos_ataque));
+                        break;
+                    }
+                    else {
+                        console.log("Si no tienes dinero pa que vienes");
+                        break;
+                    }
+                case 2:
+                    if (jugador.oro >= 15) {
+                        jugador.puntos_ataque += 10;
+                        console.log("Se a\u00F1adio AK-47 a tu inventario, tu ataque es de ".concat(jugador.puntos_ataque));
+                        break;
+                    }
+                    else {
+                        console.log("Si no tienes dinero pa que vienes");
+                        break;
+                    }
+                case 3:
+                    if (jugador.oro >= 5) {
+                        jugador.puntos_salud += 5;
+                        console.log("Te has curado,compraste una venda, tu vida es de ".concat(jugador.puntos_salud));
+                        break;
+                    }
+                    else {
+                        console.log("Si no tienes dinero pa que vienes");
+                        break;
+                    }
+                case 4:
+                    if (jugador.oro >= 10) {
+                        jugador.puntos_salud += 10;
+                        console.log("Te has curado, compraste un botiquin, tu vida es de ".concat(jugador.puntos_salud));
+                        break;
+                    }
+                    else {
+                        console.log("Si no tienes dinero pa que vienes");
+                        break;
+                    }
+                case 5:
+                    break;
+                default:
+                    console.log("Opción no válida. Por favor, elige nuevamente.");
+            }
+        } while (opcion !== 5);
+    }
+}
+Main(); //Ejecutamos el juegesito
---
+++ Práctica Tema 13 (RPG)CONSOLA/RPG.ts
@@ -0,0 +1,288 @@
+import * as readlineSync from 'readline-sync';
+
+//CLASE JUGADOR
+class Jugador {
+
+    nombre: string;
+    puntos_salud: number;
+    puntos_ataque: number;
+    oro: number;
+
+
+    constructor(nombre: string) {
+        this.nombre = nombre;
+        this.puntos_salud = 20;
+        this.puntos_ataque = 0;
+        this.oro = 2;
+
+    }
+
+
+    getNombre(): string {
+        return this.nombre;
+    }
+
+    setNombre(nuevoNombre: string): void {
+        this.nombre = nuevoNombre;
+    }
+
+    getPuntos_salud(): number {
+        return this.puntos_salud;
+    }
+
+    setPuntos_salud(nuevo_puntos_salud: number) {
+        this.puntos_salud = nuevo_puntos_salud;
+    }
+
+    getPuntos_ataque(): number {
+        return this.puntos_ataque;
+    }
+
+    setPuntos_ataque(puntos_ataque_nuevo: number) {
+        this.puntos_ataque = puntos_ataque_nuevo;
+    }
+
+    getOro(): number {
+        return this.oro;
+    }
+
+    setOro(nuevo_oro: number) {
+        this.oro = nuevo_oro;
+    }
+
+
+
+
+    imprimirAtributos(): void {
+        console.log(`Puntos de Salud: ${this.puntos_salud}`);
+        console.log(`Puntos de Ataque: ${this.puntos_ataque}`);
+        console.log(`Dinero: ${this.oro}`);
+    }
+
+    calcularFuerzaInicial(): void {
+        //Puntos de ataque aleatorios entre 1 y 15
+        this.puntos_ataque = Math.floor(Math.random() * 13) + 1;
+    }
+
+}
+
+
+//CLASE ENEMIGO
+class Enemigo {
+
+    nombre: string;
+    puntos_ataque: number;
+
+    constructor(nombre: string) {
+        this.nombre = nombre;
+        this.puntos_ataque = 0;
+
+
+    }
+
+    // Métodos get y set (opcional)
+    getNombre(): string {
+        return this.nombre;
+    }
+
+    setNombre(nombre: string): void {
+        this.nombre = nombre;
+    }
+
+    getPuntosAtaque(): number {
+        return this.puntos_ataque;
+    }
+
+    setPuntosAtaque(puntos_ataque: number): void {
+        this.puntos_ataque = puntos_ataque;
+    }
+
+    calcularFuerzaEnemigo(): void {
+        // Establecer puntos de ataque aleatorios entre 1 y 10
+        this.puntos_ataque = Math.floor(Math.random() * 10) + 1;
+    }
+
+    soltarDinero(): number {
+        // Establecer la cantidad de dinero que soltará el enemigo de forma aleatoria
+        return Math.floor(Math.random() * 10) + 1;
+    }
+
+}
+
+
+
+function Main(): void {
+
+
+
+
+
+    function historia() {
+        console.log("---------------------------------------");
+        console.log("Título del Juego: Medac`s Curse");
+        console.log("HISTORIA");
+        console.log("En la academia mágica de Medac, una maldición ha convertido a profesores");
+        console.log("y compañeros en oscuros enemigos sedientos de poder. Como estudiante");
+        console.log("resistente a la maldición, tu misión es liberar la academia y restaurar la paz.");
+        console.log("---------------------------------------");
+    }
+
+    let nombre = readlineSync.question('Ingresa tu nombre: ');
+    const jugador = new Jugador(`${nombre}`);
+    console.log(`¡Hola, ${nombre}!`);
+    jugador.calcularFuerzaInicial();
+
+
+
+    let opcion: number;
+    do {
+        historia();
+        console.log("1.Luchar contra el enemigo");
+        console.log("2.Comprar ítems");
+        console.log("3.Consultar estadisticas ");
+        console.log("4.Pagar 1 para generar nueva fuerza");
+        console.log("5.Exit");
+        opcion = readlineSync.questionInt('Ingresa tu eleccion:');
+
+        switch (opcion) {
+
+
+            case 1:
+                Luchar();
+                break;
+
+            case 2:
+                comprarItems();
+                break;
+            case 3:
+                console.log(`Nombre: ${nombre}`);
+                jugador.imprimirAtributos();
+                break;
+            case 4:
+                if (jugador.oro >= 5) {
+                    cambiarFuerza();
+                    break;
+                } else {
+                    console.log(`Si no tienes dinero pa que vienes`);
+                    break;
+                }
+            case 5:
+                console.log("Nos vemos!!!");
+                return;
+            default:
+                console.log("Opción no válida. Por favor, elige nuevamente.");
+
+
+        }
+
+
+    } while (opcion !== 5);
+
+
+    function cambiarFuerza() {
+        jugador.calcularFuerzaInicial();
+        jugador.oro -= 1;
+        console.log(`Tienes ${jugador.oro} oro`);
+    }
+
+    function Luchar() {
+
+        let enemigos: Enemigo[] = [
+            new Enemigo("Javi"),
+            new Enemigo("Evelyn"),
+            new Enemigo("Antonio")
+        ];
+
+        const nombreEnemigo = Math.floor(Math.random() * enemigos.length);//Al ser un numero el nombre sera la posicion en el array
+
+        enemigos[nombreEnemigo].calcularFuerzaEnemigo();//Calculamos su fuerza
+
+        if (jugador.puntos_salud == 0) {
+            console.log("GAME OVER");
+            return;
+        } if (jugador.puntos_ataque >= enemigos[nombreEnemigo].puntos_ataque) {
+            console.log("Has ganado!!!!!!");
+            jugador.oro += enemigos[nombreEnemigo].soltarDinero();
+            console.log(`Tienes ${jugador.oro} oro`);
+
+        } if (jugador.puntos_ataque < enemigos[nombreEnemigo].puntos_ataque) {
+            console.log("Has perdido ahahahahaa");
+            let vidaPerdida = (enemigos[nombreEnemigo].puntos_ataque) - (jugador.puntos_ataque);
+            jugador.puntos_salud -= vidaPerdida;
+            console.log(`Has perdido ${vidaPerdida} y te quedan ${jugador.puntos_salud}`)
+
+        }
+    }
+
+
+
+    function comprarItems() {
+        let opcion: number;
+        do {
+            console.log("1.Glock-18 por 5 de oro");
+            console.log("2.AK-47 por 15 de oro");
+            console.log("3.Venda por 5 de oro");
+            console.log("4.Botiquin por 10 de oro");
+            console.log("5.Exit");
+            opcion = readlineSync.questionInt('Ingresa tu eleccion:');
+
+            switch (opcion) {
+
+
+                case 1:
+                    if (jugador.oro >= 5) {
+                        jugador.puntos_ataque += 5;
+                        console.log(`Se añadio Glock-18 a tu inventario, tu ataque es de ${jugador.puntos_ataque}`);
+                        break;
+                    } else {
+                        console.log(`Si no tienes dinero pa que vienes`);
+                        break;
+                    }
+
+                case 2:
+                    if (jugador.oro >= 15) {
+                        jugador.puntos_ataque += 10;
+                        console.log(`Se añadio AK-47 a tu inventario, tu ataque es de ${jugador.puntos_ataque}`);
+                        break;
+                    } else {
+                        console.log(`Si no tienes dinero pa que vienes`);
+                        break;
+                    }
+
+                case 3:
+                    if (jugador.oro >= 5) {
+                        jugador.puntos_salud += 5;
+                        console.log(`Te has curado,compraste una venda, tu vida es de ${jugador.puntos_salud}`);
+                        break;
+                    } else {
+                        console.log(`Si no tienes dinero pa que vienes`);
+                        break;
+                    }
+
+                case 4:
+                    if (jugador.oro >= 10) {
+                        jugador.puntos_salud += 10;
+                        console.log(`Te has curado, compraste un botiquin, tu vida es de ${jugador.puntos_salud}`);
+                        break;
+                    } else {
+                        console.log(`Si no tienes dinero pa que vienes`);
+                        break;
+                    }
+
+
+                case 5:
+                    break;
+                default:
+                    console.log("Opción no válida. Por favor, elige nuevamente.");
+
+
+            }
+
+
+        } while (opcion !== 5);
+
+    }
+
+}
+
+Main();//Ejecutamos el juegesito
